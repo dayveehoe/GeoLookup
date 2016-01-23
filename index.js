949bd1b7db65cdfa3fed0a098a54512274c6f813
dayveehoe <davyho@gmail.com>
2016-01-22 19:23:12 -0600

    Adding rate limiter and json output

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 var fs = require('fs');
 var util = require('util');
 var fast_csv = require('fast-csv');
-var request = require('request');
+//var request = require('request');
+var limiter = require('simple-rate-limiter');
+var request = limiter(require('request')).to(3).per(1000).evenly(true);
+
 
 // CSV file to look up address
-var inputFile = "INPUT.csv";
-var outputFile = "OUTPUT.tsv"
+var inputFile = 'INPUT.csv';
+var outputFile = 'OUTPUT.tsv';
+var outputInJson = true;
 
 // Google API Key
-var APIKEY = fs.readFileSync('APIKEY', "utf8");
+var APIKEY = fs.readFileSync('APIKEY', 'utf8');
 
 // Command Line Usage
 if (process.argv.length == 2) {
@@ -32,13 +36,13 @@ var outputStream = fs.createWriteStream(outputFile);
 
 // Format in the style for Google Maps API
 var reformat_address = function(address) {
-    var clean_address = address.join(' ')
+    var clean_address = address
         .replace(/\s\s+/g,' ') // Remove Multiple White Spaces
         .replace(/\ /gi, '+'); // Replace Space with '+'
 
-    var url_googleapis = "https://maps.googleapis.com/maps/api/geocode/json?";
-    var url_address = "address=";
-    var url_api_key = "&key=";
+    var url_googleapis = 'https://maps.googleapis.com/maps/api/geocode/json?';
+    var url_address = 'address=';
+    var url_api_key = '&key=';
 
     // Build URL and send Google Maps request
     return url_googleapis + url_address + clean_address + url_api_key + APIKEY;
@@ -47,10 +51,10 @@ var reformat_address = function(address) {
 // Parse Google return response
 var parse_formatted_address = function(info) {
     var lat, lng;
-    if (info["results"][0] && info["results"][0]["formatted_address"]) {
+    if (info['results'][0] && info['results'][0]['formatted_address']) {
         // Fix the total size to 6 decimal positions, gives accuracy ~0.11 meters
-        lat = Number((info["results"][0]["geometry"]["location"]["lat"]).toFixed(6))
-        lng = Number((info["results"][0]["geometry"]["location"]["lng"]).toFixed(6))
+        lat = Number((info['results'][0]['geometry']['location']['lat']).toFixed(6))
+        lng = Number((info['results'][0]['geometry']['location']['lng']).toFixed(6))
     } else {
         console.log(info);
         lat = lng = 0;
@@ -59,10 +63,7 @@ var parse_formatted_address = function(info) {
 }
 
 // Lookup address with google maps api
-var address_lookup = function(address) {
-
-    // *** TODO: Add rate limiter here ***
-
+var address_lookup = function(address, outputStream) {
     // Build URL and send Google Maps request
     request(reformat_address(address), function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -72,20 +73,28 @@ var address_lookup = function(address) {
             var lat = latlng['lat'];
             var lng = latlng['lng'];
 
-            console.log(address + '\t' + lat + '\t' + lng);
-            outputStream.write(util.format(address + '\t' + lat + '\t' + lng + '\n'));
+            console.log(lat + '\t' + lng + '\t' + address);
+
+            if (outputInJson) {
+                outputStream.write(util.format({'lat':lat, 'lng':lng, 'address':address}) + ',\n');
+            }
+            else {
+                outputStream.write(util.format(lat + '\t' + lng + '\t' + address + '\n'));
+            }
+
         }
         else {
-            console.log("Error: " + error);
-            console.log("Status Code: " + response.statusCode);
+            console.log('Error: ' + error);
+            console.log('Status Code: ' + response.statusCode);
         }
     });
 }
 
+// Read CSV, for each line process the request and save it to a file.
 fast_csv
-    .fromPath(inputFile)
+    .fromPath(inputFile, {headers: true})
     .on('data', function(data) {
-        address_lookup(data);
+        address_lookup(data['address'], outputStream);
     })
     .on('end', function(){
     });
